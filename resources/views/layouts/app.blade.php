7d1a49f3dbedbeaff046b8f6d9028211aa32c412
Thierry Rietveld <thierry.rietveld0505@gmail.com>
2020-07-03 14:18:33 +0200

    navbar wrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="navbar-nav mr-auto m-navbar__ul">
                         @if(isset($catagories) && isset($team) && isset($catagory))
                             @foreach($catagories as $catagory)
                                 <li class="nav-item">
